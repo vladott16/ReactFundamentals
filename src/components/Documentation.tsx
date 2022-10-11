1ab7f687bc70df0b069d2704c761558e8fc1fc8b
vladott16 <vladott16@gmail.com>
2022-10-11 23:08:37 +0200

    typo correction

---
+++ src/components/Documentation.tsx
@@ -0,0 +1,11 @@
+  /**
+   * Returns the sum of two numbers.
+   *
+   * @param x - The first input number
+   * @param y - The second input number
+   * @returns The sum of `x` and `y`
+   *
+   */
+   function getSum(x: number, y: number): number {
+    return x + y;
+  }
